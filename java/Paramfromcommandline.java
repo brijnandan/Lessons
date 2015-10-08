b9d2e00fa7179a88b52102eb1e4bbd2617abbb63
Brij <brij@cb.com>
2015-10-08 12:25:07 +0530

    added the first program to take arguments from command line in java

---
+++ java/Paramfromcommandline.java
@@ -0,0 +1,10 @@
+
+public class Paramfromcommandline {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("first argument is:"+args[0]);
+		System.out.println("second argument is:"+args[1]);
+	}
+
+}
--- java/readme.txt
+++ java/readme.txt
@@ -1,1 +1,3 @@
 This is the file which gives info about the particular module
+
+1. Paramfromcommandline is the program which will take argumnts from command line and will execute the instruction in the main. 
