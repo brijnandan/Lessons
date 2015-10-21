47bdb66027c7c029a645a6194b4c85c098698724
Brij <brij@cb.com>
2015-10-21 11:03:43 +0530

    Shell script example1

---
+++ shell/ex1.sh
@@ -0,0 +1,35 @@
+echo " This is the shell script......"
+echo " which takes only one agrument...."
+echo " creates a file with that that argument name provides permission to the specific user"
+
+#if [ -f $1.txt ]
+#	then
+#	{
+#	echo " the file is a regular file "
+#	chmod 755 $1.txt
+#	}
+#	else
+#	echo " not a regular file "
+#fi
+
+if  [ "$#" -eq 0 ] || [ "$#" -gt 1 ] 
+	then
+	{
+	echo " the usage of the script is not correct "
+	echo " there should be exactly one command line arg"
+	echo " Usage ./ex1.sh filename "
+	}
+	else
+		touch $1.txt
+		if [ -f $1.txt ]
+			then
+			{
+				echo " the file is a regular file "
+				chmod 755 $1.txt
+			}
+			else
+				echo " not a regular file "
+		fi
+		echo " none"
+fi
+	
--- shell/readme.txt
+++ shell/readme.txt
@@ -2,3 +2,5 @@ This is the file which gives info about the particular module
 
 1. shell script added which parses pylint log file and helps in detecting the errors and warnings.
 2. authors.sh is the shell script which helps in creating the file authors.txt which helps in mapping the svn developers in git with the name and valid email id.
+3. ex 1. takes filename as an argument and add permission to that particular user, but only if file is regular.  also it shoud check if there ain't any extra arguments. Also it shoud display the usage message if wrong arguments are supplied.
+
