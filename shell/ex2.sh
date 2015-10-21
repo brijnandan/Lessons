d0f99863c8367553e0a67dbce9d3e4a8680b9ffe
Brij <brij@cb.com>
2015-10-21 16:16:18 +0530

    ex2 script added to the repo

---
+++ shell/ex2.sh
@@ -0,0 +1,11 @@
+#shell which creates a function and show its usage : 
+echo " the below method is method1"
+read num
+echo $num
+initial()
+{
+echo " enter the no of parameters"
+read num
+echo "the no of parameters are not defined "
+}
+initial
