67d51f1e7fc559ebce5402775943af87c810707e
Brij <brij@cb.com>
2015-11-04 15:48:51 +0530

    for loop and cut commmand syntax with test

---
+++ shell/ex3.sh
@@ -0,0 +1,7 @@
+cut -d ':' -f1 cat /etc/passwd  # cut command usage with a delimiter
+cut -c1-4 cat /etc/passwd       # cut comand for specific columns ie from 1 to 4
+
+
+for loop for reading the contents of a file
+for i in ` cat test.txt `
+
