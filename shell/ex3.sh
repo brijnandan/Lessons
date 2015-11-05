1a92de2bc2e03ab8e0f192208d44a225fd60801c
Brij <brij@cb.com>
2015-11-05 17:00:22 +0530

    extra commands

--- shell/ex3.sh
+++ shell/ex3.sh
@@ -4,4 +4,15 @@ cut -c1-4 cat /etc/passwd       # cut comand for specific columns ie from 1 to 4
 
 for loop for reading the contents of a file
 for i in ` cat test.txt `
+for i in {1..50}
+    do 
+        echo $i
+	    done
 
+		
+		
+		Maths:
+		
+		
+		
+		echo "expression" | bc -l   # here bc -l gives the result after the decimals
